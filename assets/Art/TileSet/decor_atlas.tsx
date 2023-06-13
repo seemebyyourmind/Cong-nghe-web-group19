ef767a7f51e4e7e10f9633629a16e0c2881199ee
Pham Viet Hoang <pvhoang1804@gmail.com>
2023-06-13 23:34:49 +0700

    re-push

--- assets/Art/TileSet/decor_atlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="decor_atlas" tilewidth="16" tileheight="16" tilecount="80" columns="10">
- <image source="C:/Users/hoang/Downloads/atlas/decor_atlas.png" width="160" height="128"/>
-</tileset>
