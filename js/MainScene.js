2d9cf89a0196ae912b03ad07d17711b3dff75dd6
Hoang <phamdinhhoang152000@gmail.com>
2023-06-17 00:05:06 +0700

    cap nhap giao dien dang ky dang nhap

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,33 +1,38 @@
 import Player from "./Player.js";
 import Prepare from "./Prepare.js";
 
-export default class MainScene extends Phaser.Scene{
-    
-    constructor(){
-        super("mainScene");
-    }
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super("mainScene");
+  }
 
-    // load asset before play
-    preload(){
-        Prepare.preload(this);
-    }
+  // load asset before play
+  preload() {
+    Prepare.preload(this);
+  }
 
-    // create and setup before first loop
-    create(){
-        console.log("Create");
-        this.player = new Player({scene:this, x: 0, y: 100, texture: 'knight_atlas', frame: 'idle_0'});
-        this.player.inputKeys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            right: Phaser.Input.Keyboard.KeyCodes.D,
-            attack: Phaser.Input.Keyboard.KeyCodes.F,
-            jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
-        })
-    }
+  // create and setup before first loop
+  create() {
+    console.log("Create");
+    this.player = new Player({
+      scene: this,
+      x: 0,
+      y: 100,
+      texture: "knight_atlas",
+      frame: "idle_0",
+    });
+    this.player.inputKeys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      attack: Phaser.Input.Keyboard.KeyCodes.F,
+      jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
+    });
+  }
 
-    // loop each frame
-    update(){
-        this.player.update();
-    }
+  // loop each frame
+  update() {
+    this.player.update();
+  }
 }
